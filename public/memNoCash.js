bc5b02ee525831983778408025d8ff1a7fd510b4
Kphillycat <keithwilliamsjr@gmail.com>
2014-02-02 08:26:00 -0500

    Update scoring system for one player

--- public/memNoCash.js
+++ public/memNoCash.js
@@ -1,11 +1,11 @@
 //Game play. Player should be able to match color and number
 $(document).ready(function(){
 	var $cards = $(".card");
-	var points;
+	var points = 0;
 
 	$cards.on("click", function(event){
 		var $this = $(this);
-		points = Number($this.find(".value").text());
+		// points = Number($this.find(".value").text());
 
 		$this.addClass("chosen");
 		if($this.has(".chosen")){
@@ -22,8 +22,14 @@ $(document).ready(function(){
 				console.log("matchy match");
 				cardTwo.addClass("played");
 				cardOne.addClass("played");
+				points += 1;
+				$(".score").find("h2").text(points);
 			} else{
 				console.log("not a matchy match");
+				cardOne.find(".color").fadeOut(2000);
+				cardTwo.find(".color").fadeOut(2000);
+				cardOne.find(".value").fadeIn(2000);
+				cardTwo.find(".value").fadeIn(2000);
 			}
 			$cards.removeClass("chosen"); //remove chosen from all the cards
 		}
